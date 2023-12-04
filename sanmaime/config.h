40a2ef794113e2d08d48391734afb5f5452856bd
te9no <tatuys9428@gmail.com>
2023-12-04 10:07:28 +0900

    fix

--- sanmaime/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define MASTER_LEFT 
-#define SPLIT_USB_DETECT
-#define TAPPING_TERM 180
-#define PERMISSIVE_HOLD
-//#define RETRO_TAPPING
-#define QUICK_TAP_TERM 0
--- sanmaime/keymaps/via/rules.mk
+++
@@ -1,4 +0,0 @@
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-ENCODER_ENABLE = yes
-SRC += a2j/translate_ansi_to_jis.c
